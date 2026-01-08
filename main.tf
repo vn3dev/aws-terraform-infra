3a7ee375b4d52a65a4619b4d3261c54de67aefce
vn3 <vinicosta.vn3dev@gmail.com>
2026-01-08 13:19:14 -0300

    deploy initial VPC with CIDR 10.0.0.0/16

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
